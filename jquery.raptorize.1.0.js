527283cf4f4258237d29dc1b2747f3b23eecdaac
melo5 <melooblack@gmail.com>
2017-11-22 13:58:14 +0100

    d

--- jquery.raptorize.1.0.js
+++ jquery.raptorize.1.0.js
@@ -2,7 +2,7 @@
  * jQuery Raptorize Plugin 1.0
  * www.ZURB.com/playground
  * Copyright 2010, ZURB
- * Free to use under the MIT license.
+ * Free to use under the MIT license. dgzhutg
  * http://www.opensource.org/licenses/mit-license.php
 */
 
@@ -12,28 +12,28 @@
     $.fn.raptorize = function(options) {
 
         //Yo' defaults
-        var defaults = {  
+        var defaults = {
             enterOn: 'click', //timer, konami-code, click
             delayTime: 5000 //time before raptor attacks on timer mode
-            };  
-        
+            };
+
         //Extend those options
-        var options = $.extend(defaults, options); 
-	
+        var options = $.extend(defaults, options);
+
         return this.each(function() {
 
 			var _this = $(this);
 			var audioSupported = false;
 			//Stupid Browser Checking which should be in jQuery Support
-			if ($.browser.mozilla && $.browser.version.substr(0, 5) >= "1.9.2" || $.browser.webkit) { 
+			if ($.browser.mozilla && $.browser.version.substr(0, 5) >= "1.9.2" || $.browser.webkit) {
 				audioSupported = true;
 			}
-			
+
 			//Raptor Vars
 			var raptorImageMarkup = '<img id="elRaptor" style="display: none" src="raptor.png" />'
-			var raptorAudioMarkup = '<audio id="elRaptorShriek" preload="auto"><source src="raptor-sound.mp3" /><source src="raptor-sound.ogg" /></audio>';	
+			var raptorAudioMarkup = '<audio id="elRaptorShriek" preload="auto"><source src="raptor-sound.mp3" /><source src="raptor-sound.ogg" /></audio>';
 			var locked = false;
-			
+
 			//Append Raptor and Style
 			$('body').append(raptorImageMarkup);
  			if(audioSupported) { $('body').append(raptorAudioMarkup); }
@@ -43,23 +43,23 @@
 				"right" : "0",
 				"display" : "block"
 			})
-			
+
 			// Animating Code
 			function init() {
 				locked = true;
-			
+
 				//Sound Hilarity
-				if(audioSupported) { 
+				if(audioSupported) {
 					function playSound() {
 						document.getElementById('elRaptorShriek').play();
 					}
 					playSound();
 				}
-								
-				// Movement Hilarity	
+
+				// Movement Hilarity
 				raptor.animate({
 					"bottom" : "0"
-				}, function() { 			
+				}, function() {
 					$(this).animate({
 						"bottom" : "-130px"
 					}, 100, function() {
@@ -76,8 +76,8 @@
 					});
 				});
 			}
-			
-			
+
+
 			//Determine Entrance
 			if(options.enterOn == 'timer') {
 				setTimeout(init, options.delayTime);
@@ -97,10 +97,9 @@
 			        	$(window).unbind('keydown.raptorz');
 			        }
 			    }, true);
-	
+
 			}
-			
+
         });//each call
     }//orbit plugin call
 })(jQuery);
-
